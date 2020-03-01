48df30528490cf5296fb39cab7e8f92c7e036481
okbp <yokookbio@gmail.com>
2020-03-01 15:38:16 +0900

    Added GOLD data for temperature_brc_nano

--- environment_organism_distribution_on_temperature_brc_nano_stanza/stanza.rb
+++ environment_organism_distribution_on_temperature_brc_nano_stanza/stanza.rb
@@ -1,6 +1,6 @@
 class EnvironmentOrganismDistributionOnTemperatureBrcNanoStanza < TogoStanza::Stanza::Base
   property :num_orgs_with_temperature_range do |meo_id|
-    results = query("http://ep.dbcls.jp/sparql-import", <<-SPARQL.strip_heredoc)
+    brc_results = query("http://ep.dbcls.jp/sparql-import", <<-SPARQL.strip_heredoc)
     PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
     PREFIX mpo: <http://purl.jp/bio/10/mpo/>
     PREFIX mccv: <http://purl.jp/bio/10/mccv#>
@@ -24,13 +24,13 @@ class EnvironmentOrganismDistributionOnTemperatureBrcNanoStanza < TogoStanza::St
         }
       }
       OPTIONAL {
-        { //nbrc
+        { # nbrc
           ?strain_id sio:SIO_000216 ?blank .
           ?blank rdf:type mpo:MPO_00102 ;
             sio:SIO_000300  ?opt_temp_brc .
         }
         UNION
-        { //jcm
+        { # jcm
           ?strain_id mccv:MCCV_000073/sio:SIO_000216 ?blank .
           ?blank a mpo:MPO_00102 ;
           sio:SIO_000300 ?opt_temp_brc .
@@ -48,7 +48,37 @@ class EnvironmentOrganismDistributionOnTemperatureBrcNanoStanza < TogoStanza::St
       }
     }
     SPARQL
-    # TODO add GOLD
+
+    gold_results = query("http://ep.dbcls.jp/sparql-import", <<-SPARQL.strip_heredoc)
+    PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+    PREFIX mpo: <http://purl.jp/bio/01/mpo#>
+    PREFIX mccv: <http://purl.jp/bio/01/mccv#>
+    PREFIX meo: <http://purl.jp/bio/11/meo/>
+
+    SELECT DISTINCT ?tax_id ?opt_temp ?min_temp ?max_temp ?l
+    FROM <http://togogenome.org/graph/gold>
+    FROM <http://togogenome.org/graph/mpo>
+    FROM <http://togogenome.org/graph/meo>
+    WHERE {
+      VALUES ?meo_mapping { meo:MEO_0000437 meo:MEO_0000440 } .
+      ?descendant rdfs:subClassOf* meo:#{meo_id} .
+      ?gold ?meo_mapping ?descendant .
+      OPTIONAL {?gold mccv:MCCV_000020 ?tax_id}
+      OPTIONAL {?tax_id mpo:MPO_10009 ?opt_temp}
+      OPTIONAL {?tax_id mpo:MPO_10010 ?min_temp}
+      OPTIONAL {?tax_id mpo:MPO_10011 ?max_temp}
+      OPTIONAL {?tax_id mpo:MPO_10003 ?growth_temp_range .
+                ?growth_temp_range rdfs:label ?label .
+                FILTER(lang(?label) = "en")
+                BIND(str(?label) AS ?l)}
+      BIND (COALESCE(?opt_temp, ?min_temp, ?max_temp,
+                     ?growth_temp_range, ?l, <NA>) AS ?temperature)
+      FILTER (regex(?tax_id, "identifiers.org") &&
+              (!(?temperature = <NA>) || bound(?growth_temp_range)))
+    }
+    SPARQL
+
+    results = brc_results.concat(gold_results)
 
     category2num = {Mesophile: 0, Thermophile: 0, Psychrophile: 0}
     results.each do |result|
@@ -80,6 +110,10 @@ class EnvironmentOrganismDistributionOnTemperatureBrcNanoStanza < TogoStanza::St
     case label
     when "Mesophile", "Psychrophile", "Thermophile"
       label.to_sym
+    when "Thermophilic"
+      :Thermophile
+    when "Hyperthermophilic"
+      :Thermophile
     when "Hyperthermophile"
       :Thermophile
     else
