5c56b3204383fe10e880807830449521926a03ea
okbp <yokookbio@gmail.com>
2020-10-28 21:32:23 +0900

    query tuning

--- organism_pathogen_information_stanza/stanza.rb
+++ organism_pathogen_information_stanza/stanza.rb
@@ -4,24 +4,24 @@ class OrganismPathogenInformationStanza < TogoStanza::Stanza::Base
       PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
       PREFIX pdo: <http://purl.jp/bio/11/pdo/>
       PREFIX taxid: <http://identifiers.org/taxonomy/>
+      PREFIX ddbj_tax: <http://ddbj.nig.ac.jp/ontologies/taxonomy/>
 
       SELECT (REPLACE(STR(?tax_id),"http://identifiers.org/taxonomy/","") AS ?tax_no)
        ?bacterialName (GROUP_CONCAT(DISTINCT ?diseaseName; SEPARATOR = ", ") AS ?diseaseNameSet) ?infectiousType ?strainType
+      FROM <http://togogenome.org/graph/taxonomy>
+      FROM <http://togogenome.org/graph/pdo_mapping>
+      FROM <http://togogenome.org/graph/pdo>
       WHERE
       {
-        GRAPH <http://togogenome.org/graph/taxonomy> {
-          ?tax_id rdfs:subClassOf* taxid:#{tax_id} .
-          ?tax_id rdfs:label ?bacterialName .
-        }
-        GRAPH <http://togogenome.org/graph/pdo_mapping> {
-          ?tax_id pdo:isAssociatedTo ?blank .
-          ?blank ?p ?disease FILTER (?p IN(pdo:mayCaused, pdo:isRelatedTo)).
-          OPTIONAL { ?tax_id pdo:isAssociatedTo/pdo:infectiousType ?infectiousType . }
-          OPTIONAL { ?tax_id pdo:isAssociatedTo/pdo:strainType ?strainType . }
-        }
-        GRAPH <http://togogenome.org/graph/pdo> {
-          ?disease rdfs:label ?diseaseName .
-        }
+        VALUES ?search_tax { taxid:#{tax_id} }
+        ?search_tax a ddbj_tax:Taxon .
+        ?tax_id rdfs:subClassOf* ?search_tax ;
+          rdfs:label ?bacterialName ;
+          pdo:isAssociatedTo ?blank .
+        ?blank ?p ?disease FILTER (?p IN(pdo:mayCaused, pdo:isRelatedTo)).
+        OPTIONAL { ?tax_id pdo:isAssociatedTo/pdo:infectiousType ?infectiousType . }
+        OPTIONAL { ?tax_id pdo:isAssociatedTo/pdo:strainType ?strainType . }
+        ?disease rdfs:label ?diseaseName .
       } GROUP BY ?tax_id ?bacterialName ?infectiousType ?strainType
     SPARQL
     results.map {|hash|
